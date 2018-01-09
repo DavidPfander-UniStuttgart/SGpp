8c295094ad8b830966c299d1890525ae76160cc4
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-01-09 14:39:45 +0100

    added test and faster SoA conversion template

--- AutoTuneTMP_kernels/streaming_mult_kernel.cpp
+++ AutoTuneTMP_kernels/streaming_mult_kernel.cpp
@@ -22,9 +22,10 @@ using reg_array = register_array<double_v, DATA_BLOCKING>;
 // sgpp::base::Grid& grid, base::DataMatrix& dataset, sgpp::base::DataVector& alpha,
 // sgpp::base::DataVector& result
 
-extern "C" void streaming_mult_kernel(size_t dims, sgpp::base::Grid& grid,
-                                      sgpp::base::DataMatrix& dataset,
-                                      sgpp::base::DataVector& alpha, sgpp::base::DataVector& result)
+extern "C" sgpp::base::DataVector streaming_mult_kernel(sgpp::base::Grid& grid,
+                                                        sgpp::base::DataMatrix& dataset,
+                                                        sgpp::base::DataVector& alpha,
+                                                        double& duration_compute)
 
 {
   ////////////////////////////////////////
@@ -35,27 +36,28 @@ extern "C" void streaming_mult_kernel(size_t dims, sgpp::base::Grid& grid,
   // grid as non-SoA for testing
   std::vector<double, boost::alignment::aligned_allocator<double, 32>> level_list;
   std::vector<double, boost::alignment::aligned_allocator<double, 32>> index_list;
-  level_list.resize(storage.getSize() * dims);
-  index_list.resize(storage.getSize() * dims);
+  level_list.resize(storage.getSize() * DIMS);
+  index_list.resize(storage.getSize() * DIMS);
   for (size_t i = 0; i < storage.getSize(); i++) {
     sgpp::base::HashGridPoint& point = storage[i];
-    for (size_t d = 0; d < dims; d++) {
+    for (size_t d = 0; d < DIMS; d++) {
       sgpp::base::GridPoint::level_type level;
       sgpp::base::GridPoint::index_type index;
       point.get(d, level, index);
-      level_list[i * dims + d] = static_cast<double>(1 << level);
-      index_list[i * dims + d] = static_cast<double>(index);
+      level_list[i * DIMS + d] = static_cast<double>(1 << level);
+      index_list[i * DIMS + d] = static_cast<double>(index);
     }
   }
 
   // data as SoA
-  size_t padding = dataset.getNrows() % (DATA_BLOCKING * double_v::size()) == 0
-                       ? 0
-                       : (DATA_BLOCKING * double_v::size()) -
-                             (dataset.getNrows() % (DATA_BLOCKING * double_v::size()));
-  size_t dataset_size = dataset.getNrows() + padding;
-  opttmp::memory_layout::struct_of_array_data<double_v, double, sgpp::base::DataMatrix> dataset_SoA(
-      dataset, dims, dataset.getNrows(), padding);
+  constexpr size_t padding =
+      ENTRIES % (DATA_BLOCKING * double_v::size()) == 0
+          ? 0
+          : (DATA_BLOCKING * double_v::size()) - (ENTRIES % (DATA_BLOCKING * double_v::size()));
+  const size_t dataset_size = dataset.getNrows() + padding;
+  opttmp::memory_layout::struct_of_array_data<double_v, sgpp::base::DataMatrix, DIMS, ENTRIES,
+                                              padding>
+      dataset_SoA(dataset);
 
   // size_t dataset_size = dataset.getNrows();
   // dataset_size += dataset_size % (DATA_BLOCKING * double_v::size()) == 0
@@ -92,7 +94,7 @@ extern "C" void streaming_mult_kernel(size_t dims, sgpp::base::Grid& grid,
     for (size_t j = 0; j < alpha.size(); j++) {
       reg_array evalNds_arr(alpha[j]);  // broadcast
 
-      for (size_t d = 0; d < dims; d++) {
+      for (size_t d = 0; d < DIMS; d++) {
         // non-SoA is faster (2 streams, instead of 2d streams)
         // 2^l * x - i (level_list_SoA stores 2^l, not l)
         // double_v level_dim = level_list[j * dims + d];  // broadcasts
@@ -105,8 +107,8 @@ extern "C" void streaming_mult_kernel(size_t dims, sgpp::base::Grid& grid,
         // converted to FMA through expression templates
         // (also mixes array and non-array type vector variables)
         // temps_arr = (data_dims_arr * level_dim) - index_dim;  // 2 FLOP
-        temps_arr = (data_dims_arr * double_v(level_list[j * dims + d])) -
-                    double_v(index_list[j * dims + d]);  // 2 FLOP
+        temps_arr = (data_dims_arr * double_v(level_list[j * DIMS + d])) -
+                    double_v(index_list[j * DIMS + d]);  // 2 FLOP
 
         reg_array eval1ds_arr;
         eval1ds_arr = opttmp::vectorization::max(
@@ -120,18 +122,20 @@ extern "C" void streaming_mult_kernel(size_t dims, sgpp::base::Grid& grid,
     result_temps_arr.memstore(&result_padded[i], Vc::flags::vector_aligned);
   }
 
+  auto end = std::chrono::high_resolution_clock::now();
+
+  sgpp::base::DataVector result(dataset.getNrows());
   for (size_t i = 0; i < result.size(); i++) {
     result[i] = result_padded[i];
   }
 
-  auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = end - start;
-  double duration = diff.count();
-  double total_flops = dataset_size * alpha.size() * (6 * dims + 1);
-  std::cout << "duration (inner): " << duration << std::endl;
-  std::cout << "gflops (inner): " << ((total_flops * 1E-9) / duration) << std::endl;
+  duration_compute = diff.count();
+  double total_flops = dataset_size * alpha.size() * (6 * DIMS + 1);
+  std::cout << "duration_compute: " << duration_compute << std::endl;
+  std::cout << "gflops_compute: " << ((total_flops * 1E-9) / duration_compute) << std::endl;
 
-  // print flops
+  return result;
 }
 
 // extern "C" void streaming_mult_kernel(size_t dims, std::vector<double> &dataset_SoA,
