2f0093488c860c2e035c33ec575e9eda746a1679
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-06-21 16:30:45 +0200

    improving configuration

--- source_autotunetmp_pcsgs09.sh
+++ source_autotunetmp_pcsgs09.sh
@@ -1,4 +1,5 @@
 # to be sourced from shell to setup the environment
+export LD_LIBRARY_PATH=/home/pfandedd/git/SGpp/lib/sgpp:$LD_LIBRARY_PATH
 export SGPP_BASE_INCLUDE_DIR=/home/pfandedd/git/SGPP_debug/base/src
 export AUTOTUNETMP_INCLUDE_DIR=/home/pfandedd/git/AutoTuneTMP/AutoTuneTMP_install_debug/include
 export VC_INCLUDE_DIR=/home/pfandedd/git/AutoTuneTMP/Vc_install/include
