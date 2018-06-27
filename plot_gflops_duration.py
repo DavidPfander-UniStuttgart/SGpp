0eecf478f7c290e6b840829639ee257e7c6a4c03
fire <fire@element.com>
2018-06-27 23:20:59 +0200

    up plot scripts

--- plot_gflops_duration.py
+++ plot_gflops_duration.py
@@ -4,15 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 import pylab
+import argparse
 
+parser = argparse.ArgumentParser(description='Plot graphs')
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument('--device_name', required=True)
+requiredNamed.add_argument('--precision', required=True)
 clusters=3
-deviceName="config_ocl_float_i76700k.cfg" # i76700k
-precision="float"
+args = parser.parse_args()
 
-# for dim in range(2, 3, 2):
+kernels_gflops = ['avr_gflops_generate_b', 'avr_gflops_density', 'gflops_create_graph', 'gflops_prune_graph']
+kernels_duration = ['total_duration_generate_b', 'total_duration_density', 'duration_create_graph', 'duration_prune_graph']
+kernels_print_name = {'avr_gflops_generate_b': " density right-hand side ", 'avr_gflops_density': " density matrix-vector product ", 'gflops_create_graph': " create kNN graph ", 'gflops_prune_graph': " prune kNN graph "}
+kernels_duration_print_name = {'total_duration_generate_b': " density right-hand side ", 'total_duration_density': " density matrix-vector product ", 'duration_create_graph': " create kNN graph ", 'duration_prune_graph': " prune kNN graph "}
+
 for dim in range(2, 11, 2):
 
-    results_file = "results/results_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + deviceName + "_" + precision + ".csv"
+    results_file = "results/results_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".csv"
 
     csvfile = open(results_file, 'r')
     reader = csv.reader(csvfile, delimiter=';', quotechar='#')
@@ -24,36 +32,54 @@ for dim in range(2, 11, 2):
         for field_index in range(len(headers)):
             columns[field_index] += [float(row[field_index])]
 
-    # for row in columns:
-    #     print(row)
-
     header_map = {}
     counter = 0
     for header in headers:
         header_map[header] = counter
         counter += 1
 
-    print(columns[0])
-    print(columns[3])
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['avr_gflops_generate_b']], label='avr_gflops_generate_b')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['avr_gflops_density']], label='avr_gflops_density')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['gflops_create_graph']], label='gflops_create_graph')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['gflops_prune_graph']], label='gflops_prune_graph')
+    for kernel_gflops in kernels_gflops:
+        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_gflops]], label=kernel_gflops)
     pylab.legend(loc='upper left')
-    # pylab.show()
-    pylab.savefig("graphs/" + "gflops_gaussian_c" + str(clusters) + "_dim" + str(dim) + ".png")
+    pylab.title("Performance for gaussian dataset, clusters = " + str(clusters) + ", dim = " + str(dim))
+    pylab.xlabel("dataset size")
+    pylab.ylabel("GFLOPS")
+    pylab.savefig("graphs/" + "gflops_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".png")
     pylab.clf()
 
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['total_duration_generate_b']], label='avr_duration_generate_b')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['total_duration_density']], label='avr_duration_density')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['duration_create_graph']], label='duration_create_graph')
-    pylab.plot(columns[header_map['dataset_size']], columns[header_map['duration_prune_graph']], label='duration_prune_graph')
+    for kernel_duration in kernels_duration:
+        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_duration]], label=kernel_duration)
+
     pylab.legend(loc='upper left')
-    # pylab.show()
-    pylab.savefig("graphs/" + "duration_gaussian_c" + str(clusters) + "_dim" + str(dim) + ".png")
+    pylab.title("Duration for gaussian dataset, clusters = " + str(clusters) + ", dim = " + str(dim))
+    pylab.xlabel("dataset size")
+    pylab.ylabel("duration (s)")
+    pylab.savefig("graphs/" + "duration_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".png")
     pylab.clf()
 
-    # fig, ax = plt.figure()
-    # ax.plot(columns[header_map['dataset_size']], header_map['avr_gflops_generate_b'], label='avr_gflops_generate_b')
-    # ax.legend()
-    # fig.show()
+for kernel_gflops in kernels_gflops:
+    for dim in range(2, 11, 2):
+        results_file = "results/results_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".csv"
+        csvfile = open(results_file, 'r')
+        reader = csv.reader(csvfile, delimiter=';', quotechar='#')
+        headers = next(reader, None)
+        columns = [[] for i in range(len(headers))]
+        row_count = 0
+        for row in reader:
+            for field_index in range(len(headers)):
+                columns[field_index] += [float(row[field_index])]
+
+        header_map = {}
+        counter = 0
+        for header in headers:
+            header_map[header] = counter
+            counter += 1
+
+        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_gflops]], label="dim " + str(dim))
+
+    pylab.legend(loc='upper left')
+    pylab.title("Performance of " + kernels_print_name[kernel_gflops] + " kernel")
+    pylab.xlabel("dataset size")
+    pylab.ylabel("GFLOPS")
+    pylab.savefig("graphs/" + "gflops_" + kernel_gflops + "_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".png")
+    pylab.clf()
