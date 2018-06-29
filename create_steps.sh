7a6175a4a52119f5ce56102e9c22c80c9d0975e0
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-06-29 09:39:12 +0200

    before merge

--- create_steps.sh
+++ create_steps.sh
@@ -12,19 +12,6 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
-# scenarioName=$1
-# echo "scenarioName: $scenarioName"
-# datasetFileName=$2
-# echo "datasetFileName: $datasetFileName"
-# level=$3
-# echo "level: $level"
-# lambda=$4
-# echo "lambda: $lambda"
-# threshold=$5
-# echo "threshold: $threshold"
-# k=$6
-# echo "k: k"
-
 # # ripley dataset
 # scenarioName="ripley_$deviceName"
 # echo "scenarioName: $scenarioName"
@@ -52,15 +39,15 @@ scenarioName="gaussian_plotable_$deviceName"
 echo "scenarioName: $scenarioName"
 datasetFileName="datasets/gaussian_c3_size200_dim2.arff"
 echo "datasetFileName: $datasetFileName"
-level="4"
+level="3"
 echo "level: $level"
-lambda="1E-4"
+lambda="2.5E-2"
 echo "lambda: $lambda"
-threshold="0.2"
+threshold="0.65"
 echo "threshold: $threshold"
 k="5"
 echo "k: k"
-./datadriven/examplesOCL/clustering_cmd --datasetFileName $datasetFileName --level $level --lambda $lambda --threshold $threshold --k $k --config $config --write_graphs $scenarioName --density_eval_full_grid_level $eval_grid_level
+./datadriven/examplesOCL/clustering_cmd --datasetFileName $datasetFileName --level $level --lambda $lambda --threshold $threshold --k $k --config $config --write_graphs $scenarioName --density_eval_full_grid_level $eval_grid_level --refinement_steps 4 --refinement_points 10 --coarsen_points 999 --coarsen_threshold 0.01
 
 ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn --dont_display_data
 ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn
@@ -69,26 +56,28 @@ echo "k: k"
 ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName
 ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --use_unpruned_graph --dont_display_grid
 ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid
+./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn --dont_display_density
+./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_data --dont_display_knn
 
-# plotable clusters dataset
-scenarioName="gaussian_plotable_large_$deviceName"
-echo "scenarioName: $scenarioName"
-datasetFileName="datasets/gaussian_c3_size20000_dim2.arff"
-echo "datasetFileName: $datasetFileName"
-level="4"
-echo "level: $level"
-lambda="1E-4"
-echo "lambda: $lambda"
-threshold="0.2"
-echo "threshold: $threshold"
-k="5"
-echo "k: k"
-./datadriven/examplesOCL/clustering_cmd --datasetFileName $datasetFileName --level $level --lambda $lambda --threshold $threshold --k $k --config $config --write_graphs $scenarioName --density_eval_full_grid_level $eval_grid_level
+# # plotable clusters dataset
+# scenarioName="gaussian_plotable_large_$deviceName"
+# echo "scenarioName: $scenarioName"
+# datasetFileName="datasets/gaussian_c3_size20000_dim2.arff"
+# echo "datasetFileName: $datasetFileName"
+# level="4"
+# echo "level: $level"
+# lambda="1E-4"
+# echo "lambda: $lambda"
+# threshold="0.2"
+# echo "threshold: $threshold"
+# k="5"
+# echo "k: k"
+# ./datadriven/examplesOCL/clustering_cmd --datasetFileName $datasetFileName --level $level --lambda $lambda --threshold $threshold --k $k --config $config --write_graphs $scenarioName --density_eval_full_grid_level $eval_grid_level
 
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn --dont_display_data
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_knn
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --use_unpruned_graph
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --use_unpruned_graph --dont_display_grid
-./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn --dont_display_data
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid --dont_display_knn
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_knn
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --use_unpruned_graph
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --use_unpruned_graph --dont_display_grid
+# ./create_clustering_graphs.py --eval_grid_level $eval_grid_level --scenario_name $scenarioName --dataset_name $datasetFileName --dont_display_grid
--- plot_gflops_duration.py
+++ plot_gflops_duration.py
@@ -2,8 +2,18 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+plt.rc('text', usetex=True)
+font = {'family' : 'Times',
+        'weight' : 'normal',
+        'size'   : 8}
+plt.rc('font', **font)
+
 import csv
 import pylab
+from pylab import rcParams
+rcParams['figure.figsize'] = 4.5, 3.2
+# rcParams.update({'figure.autolayout': True})
 import argparse
 
 parser = argparse.ArgumentParser(description='Plot graphs')
@@ -39,25 +49,27 @@ for dim in range(2, 11, 2):
         counter += 1
 
     for kernel_gflops in kernels_gflops:
-        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_gflops]], label=kernel_gflops)
+        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_gflops]], label=kernels_print_name[kernel_gflops])
     pylab.legend(loc='upper left')
     pylab.title("Performance for gaussian dataset, clusters = " + str(clusters) + ", dim = " + str(dim))
     pylab.xlabel("dataset size")
     pylab.ylabel("GFLOPS (" + str(args.precision) + ")")
-    pylab.savefig("graphs/" + "gflops_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".png")
+    pylab.savefig("graphs/" + "gflops_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".eps")
     pylab.clf()
 
     for kernel_duration in kernels_duration:
-        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_duration]], label=kernel_duration)
+        pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_duration]], label=kernels_duration_print_name[kernel_duration])
 
     pylab.legend(loc='upper left')
     pylab.title("Duration for gaussian dataset, clusters = " + str(clusters) + ", dim = " + str(dim) + ", " + args.precision)
     pylab.xlabel("dataset size")
     pylab.ylabel("duration (s)")
-    pylab.savefig("graphs/" + "duration_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".png")
+    pylab.savefig("graphs/" + "duration_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".eps")
     pylab.clf()
 
 for kernel_gflops in kernels_gflops:
+    pylab.tight_layout()
+
     for dim in range(2, 11, 2):
         results_file = "results/results_gaussian_c" + str(clusters) + "_dim" + str(dim) + "_" + args.device_name + "_" + args.precision + ".csv"
         csvfile = open(results_file, 'r')
@@ -77,9 +89,14 @@ for kernel_gflops in kernels_gflops:
 
         pylab.plot(columns[header_map['dataset_size']], columns[header_map[kernel_gflops]], label="dim " + str(dim))
 
-    pylab.legend(loc='upper left')
+    # pylab.legend(loc='upper left')
+    # pylab.legend(loc='best', fancybox=True, framealpha=0.5)
+    pylab.legend(loc='upper center', bbox_to_anchor=(0.5, -0.14), ncol=5)
+    # pylab.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     pylab.title("Performance of " + kernels_print_name[kernel_gflops] + " kernel")
     pylab.xlabel("dataset size")
     pylab.ylabel("GFLOPS (" + str(args.precision) + ")")
-    pylab.savefig("graphs/" + "kernel_" + kernel_gflops + "_gflops_gaussian_c" + str(clusters) + "_" + args.device_name + "_" + args.precision + ".png")
+    plt.gcf().subplots_adjust(bottom=0.19)
+
+    pylab.savefig("graphs/" + "kernel_" + kernel_gflops + "_gflops_gaussian_c" + str(clusters) + "_" + args.device_name + "_" + args.precision + ".eps")
     pylab.clf()
