a7871e0d97b8ae6d501e63b197959560122481da
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-07-19 15:19:39 +0200

    better experiments

--- run_all_experiments_high_dim.py
+++ run_all_experiments_high_dim.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
-# import sys
 import shlex
 import subprocess
 import re
-# from itertools import chain
-
 import argparse
 parser = argparse.ArgumentParser(description='Run all high dim experiments.')
 parser.add_argument('--with-compression', dest='with_compression', action='store_true')
+parser.set_defaults(with_compression=False)
+parser.add_argument('--fixed-grid-points', dest='fixed_grid_points', action='store_true')
 parser.set_defaults(with_compression=False)
 requiredNamed = parser.add_argument_group('required arguments')
 requiredNamed.add_argument('--device_name', required=True)
 requiredNamed.add_argument('--config_file', required=True)
 requiredNamed.add_argument('--precision', required=True)
+
 args = parser.parse_args()
 
 print("config_file: " + args.config_file)
@@ -27,9 +27,12 @@ print("with_compression: " + str(args.with_compression))
 # common
 # levels = {2: 8, 4: 7, 6: 6, 8: 5, 10: 4}
 # levels = {2: 5, 4: 5, 6: 5, 8: 5, 10: 5}
-levels = [4]
 
-print("levels: " + str(levels))
+base_dim = 4
+base_dim_level = 9
+level = 4
+
+print("levels: " + str(level))
 lambdas = 1E-2
 print("lambda: " + str(lambdas))
 
@@ -42,8 +45,11 @@ coarsen_threshold=1E-3
 CSV_SEP = ";"
 
 # for dim in range(2, 3, 2):
-for level in levels:
-    resultsFileName = "results/results_friedman2_high_dim_gaussian_" + args.device_name + "_" + args.precision + "_compression" + str(args.with_compression) + ".csv"
+for level in [level]:
+    if args.fixed_grid_points:
+        resultsFileName = "results/results_friedman2_high_dim_gaussian_" + args.device_name + "_" + args.precision + "_compression" + str(args.with_compression) + "_fixed_dim" + str(base_dim) + ".csv"
+    else:
+        resultsFileName = "results/results_friedman2_high_dim_gaussian_" + args.device_name + "_" + args.precision + "_compression" + str(args.with_compression) + ".csv"
     print("resultsFileName:", resultsFileName)
     f_result = open(resultsFileName, "w")
     f_result.write("dim" + CSV_SEP + "dataset_size" + CSV_SEP + "refinement_steps" + CSV_SEP + "total_duration_generate_b" + CSV_SEP + "avr_gflops_generate_b" + CSV_SEP + "total_duration_density" + CSV_SEP + "avr_gflops_density\n")
@@ -54,7 +60,10 @@ for level in levels:
         for dataset_size in [500000]:
            dataset_file_name = "datasets/friedman/friedman2_filled_dim" + str(dim) + "_" + str(dataset_size) + ".arff"
            print("experiments for " + dataset_file_name)
-           cmd = "./datadriven/examplesOCL/density_cmd --datasetFileName " + dataset_file_name + " --level " + str(level) + " --lambda " + str(lambdas) + " --config " + args.config_file + " --refinement_steps " + str(refinement_steps) + " --refinement_points " + str(refinement_points) + " --coarsen_points " + str(coarsen_points) + " --coarsen_threshold " + str(coarsen_threshold)
+           if args.fixed_grid_points:
+               cmd = "./datadriven/examplesOCL/density_cmd_filled_dim --datasetFileName " + dataset_file_name + " --base_dim " + str(base_dim) + " --base_dim_level " + str(base_dim_level) + " --lambda " + str(lambdas) + " --config " + args.config_file + " --refinement_steps " + str(refinement_steps) + " --refinement_points " + str(refinement_points) + " --coarsen_points " + str(coarsen_points) + " --coarsen_threshold " + str(coarsen_threshold)
+           else:
+               cmd = "./datadriven/examplesOCL/density_cmd --datasetFileName " + dataset_file_name + " --level " + str(level) + " --lambda " + str(lambdas) + " --config " + args.config_file + " --refinement_steps " + str(refinement_steps) + " --refinement_points " + str(refinement_points) + " --coarsen_points " + str(coarsen_points) + " --coarsen_threshold " + str(coarsen_threshold)
            print("cmd: " + cmd)
            p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
            output = p.communicate()[0]
--- run_compression_experiments.sh
+++ run_compression_experiments.sh
@@ -9,5 +9,8 @@ if [ $# -eq 0 ]; then
 fi
 
 source source_autotunetmp_`hostname`.sh
+./run_all_experiments_high_dim.py --device_name $deviceName --config_file config_ocl_float_${deviceName}_compression.cfg --precision float --with-compression --fixed-grid-points
+./run_all_experiments_high_dim.py --device_name ${deviceName} --config_file config_ocl_float_${deviceName}_no_compression.cfg --precision float --fixed-grid-points
+
 ./run_all_experiments_high_dim.py --device_name $deviceName --config_file config_ocl_float_${deviceName}_compression.cfg --precision float --with-compression
 ./run_all_experiments_high_dim.py --device_name ${deviceName} --config_file config_ocl_float_${deviceName}_no_compression.cfg --precision float
