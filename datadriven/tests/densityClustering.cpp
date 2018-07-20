d21aed31487031e0879fb29ca425d80eec92bae3
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-07-20 17:55:14 +0200

    Added compression unit test for unsigned int

--- datadriven/tests/densityClustering.cpp
+++ datadriven/tests/densityClustering.cpp
@@ -165,7 +165,6 @@ BOOST_AUTO_TEST_CASE(DensityRHSOpenCL) {
     for (size_t i = 0; i < gridsize; ++i) {
       BOOST_CHECK_CLOSE(rhs_optimal_result[i], b[i], 0.001);
     }
-
   }
 
   {
@@ -173,14 +172,14 @@ BOOST_AUTO_TEST_CASE(DensityRHSOpenCL) {
     std::shared_ptr<sgpp::base::OCLOperationConfiguration> parameters =
         getConfigurationDefaultsSingleDevice();
     sgpp::datadriven::DensityOCLMultiPlatform::OperationDensity::load_default_parameters(parameters);
-    std::cout << "Testing rhs kernel with compression and without local memory ..." << std::endl;
+    std::cout << "Testing rhs kernel with local memory and compression ..." << std::endl;
     for (std::string &platformName : (*parameters)["PLATFORMS"].keys()) {
       json::Node &platformNode = (*parameters)["PLATFORMS"][platformName];
       for (std::string &deviceName : platformNode["DEVICES"].keys()) {
         json::Node &deviceNode = platformNode["DEVICES"][deviceName];
         const std::string &kernelName = "cscheme";
         json::Node &kernelNode = deviceNode["KERNELS"][kernelName];
-        kernelNode.replaceIDAttr("KERNEL_USE_LOCAL_MEMORY", false);
+        kernelNode.replaceIDAttr("KERNEL_USE_LOCAL_MEMORY", true);
         kernelNode.replaceIDAttr("USE_COMPRESSION_FIXED", true);
       }
     }
@@ -196,7 +195,37 @@ BOOST_AUTO_TEST_CASE(DensityRHSOpenCL) {
     for (size_t i = 0; i < gridsize; ++i) {
       BOOST_CHECK_CLOSE(rhs_optimal_result[i], b[i], 0.001);
     }
+  }
 
+  {
+    // Create OCL configuration
+    std::shared_ptr<sgpp::base::OCLOperationConfiguration> parameters =
+        getConfigurationDefaultsSingleDevice();
+    sgpp::datadriven::DensityOCLMultiPlatform::OperationDensity::load_default_parameters(parameters);
+    std::cout << "Testing rhs kernel with unsigned compression type unsigned int and with local memory ..." << std::endl;
+    for (std::string &platformName : (*parameters)["PLATFORMS"].keys()) {
+      json::Node &platformNode = (*parameters)["PLATFORMS"][platformName];
+      for (std::string &deviceName : platformNode["DEVICES"].keys()) {
+        json::Node &deviceNode = platformNode["DEVICES"][deviceName];
+        const std::string &kernelName = "cscheme";
+        json::Node &kernelNode = deviceNode["KERNELS"][kernelName];
+        kernelNode.replaceIDAttr("KERNEL_USE_LOCAL_MEMORY", false);
+        kernelNode.replaceIDAttr("USE_COMPRESSION_FIXED", true);
+        kernelNode.replaceIDAttr("COMPRESSION_TYPE", "unsigned int");
+      }
+    }
+    // Create OpenCL Manager
+    auto manager = std::make_shared<sgpp::base::OCLManagerMultiPlatform>(parameters);
+    // Create operation
+    auto operation_rhs = std::make_unique<
+      sgpp::datadriven::DensityOCLMultiPlatform::OperationDensityOCLMultiPlatform<double>>(
+          *grid, 2, manager, parameters, 0.001);
+
+    sgpp::base::DataVector b(gridsize);
+    operation_rhs->generateb(dataset, b);
+    for (size_t i = 0; i < gridsize; ++i) {
+      BOOST_CHECK_CLOSE(rhs_optimal_result[i], b[i], 0.001);
+    }
   }
 
   std::cout << "Finished rhs kernel tests" << std::endl << std::endl;
