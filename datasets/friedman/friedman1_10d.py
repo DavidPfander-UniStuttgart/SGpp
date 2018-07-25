2da20d1166857f193b13178ac194eebf7a8bbe3c
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-07-25 22:25:59 +0200

    up

--- datasets/friedman/friedman1_10d.py
+++ datasets/friedman/friedman1_10d.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f36e92bb89c48b289fc19a9056fe350f154e0811c58abfe04c4f605eb2c0664b
-size 2102
+oid sha256:036637df1567291345cf5e3b60388ebfe23c3db9cbc7ca932d22dcbf1dc5d75d
+size 2103
