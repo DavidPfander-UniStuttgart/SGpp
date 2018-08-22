5c13846d52aa6e7524dbb61e5d04c625be962a7d
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-08-22 15:48:05 +0200

    Readded clustering_cmd arguments required by distributed python test

--- datadriven/examplesOCL/clustering_cmd.cpp
+++ datadriven/examplesOCL/clustering_cmd.cpp
@@ -124,6 +124,12 @@ int main(int argc, char **argv) {
   bool record_timings;
   std::string scenario_name;
 
+  std::string cluster_file = "";
+  std::string rhs_erg_filename = "";
+  std::string density_coefficients_filename = "";
+  std::string knn_filename = "";
+  std::string pruned_knn_filename = "";
+
   std::string compare_knn_csv_file_name;
 
   size_t refinement_steps;
@@ -196,6 +202,21 @@ int main(int argc, char **argv) {
       boost::program_options::value<bool>(&write_knn_graph)
           ->default_value(false),
       "write the knn graph calculated to a csv-file")(
+      "cluster_file",
+      boost::program_options::value<std::string>(&cluster_file)->default_value(""),
+      "Output file for the detected clusters. None if empty.")(
+      "rhs_erg_file",
+      boost::program_options::value<std::string>(&rhs_erg_filename),
+      "Filename where the final rhs values will be written.")(
+      "density_coefficients_file",
+      boost::program_options::value<std::string>(&density_coefficients_filename),
+      "Filename where the final grid coefficients for the density function will be written.")(
+      "knn_file",
+      boost::program_options::value<std::string>(&knn_filename),
+      "Filename for the knn graph")(
+      "pruned_knn_file",
+      boost::program_options::value<std::string>(&pruned_knn_filename),
+      "Filename for the pruned knn graph")(
       "write_pruned_knn_graph",
       boost::program_options::value<bool>(&write_pruned_knn_graph)
           ->default_value(false),
@@ -570,6 +591,23 @@ int main(int argc, char **argv) {
     alpha[i] = alpha[i] * 1.0 / (max - min);
   }
 
+  // Output final rhs values
+  if (rhs_erg_filename != "") {
+    std::ofstream out_rhs(rhs_erg_filename);
+    for (size_t i = 0; i < grid->getSize(); ++i) {
+      out_rhs << b[i] << std::endl;
+    }
+    out_rhs.close();
+  }
+  // Output final coefficients
+  if (density_coefficients_filename != "") {
+    std::ofstream out_coefficients(density_coefficients_filename);
+    for (size_t i = 0; i < grid->getSize(); ++i) {
+      out_coefficients << alpha[i] << std::endl;
+    }
+    out_coefficients.close();
+  }
+
   if (write_density_grid) {
     std::ofstream out_grid(std::string("results/") + scenario_name +
                            "_grid_coord.csv");
@@ -796,6 +834,29 @@ int main(int argc, char **argv) {
                               graph, k);
     }
   }
+  auto print_knn_graph = [&trainingData, k](std::string filename, std::vector<int> &graph) {
+    std::ofstream out_graph(filename);
+    for (size_t i = 0; i < trainingData.getNrows(); ++i) {
+      bool first = true;
+      for (size_t j = 0; j < k; ++j) {
+        if (graph[i * k + j] == -1) {
+          continue;
+        }
+        if (!first) {
+          out_graph << ", ";
+        } else {
+          first = false;
+        }
+        out_graph << graph[i * k + j];
+      }
+      out_graph << std::endl;
+    }
+    out_graph.close();
+  };
+  // output for opencl/mpi comparison script
+  if (knn_filename != "") {
+    print_knn_graph(knn_filename, graph);
+  }
 
   {
     std::cout << "Finding clusters..." << std::endl;
@@ -821,6 +882,14 @@ int main(int argc, char **argv) {
     if (write_cluster_map) {
       std::ofstream out_cluster_map(std::string("results/") + scenario_name +
                                     "_cluster_map.csv");
+      for (size_t i = 0; i < trainingData.getNrows(); ++i) {
+        out_cluster_map << node_cluster_map[i] << std::endl;
+      }
+      out_cluster_map.close();
+    }
+    // Keep for compatibility for extern scripts
+    if (cluster_file != "") {
+      std::ofstream out_cluster_map(cluster_file);
       for (size_t i = 0; i < trainingData.getNrows(); ++i) {
         out_cluster_map << node_cluster_map[i] << std::endl;
       }
