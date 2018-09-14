6d9b2573076d3314749a925a93c93ba8a47dbc55
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-09-14 13:31:09 +0200

    demo results

--- run_piz_daint_clustering.py
+++ run_piz_daint_clustering.py
@@ -7,11 +7,11 @@ nodes_start = 1
 nodes_end = 8
 config_file = "config_ocl_float_P100.cfg"
 # config_file = "config_ocl_float_XeonE31585v5.cfg"
-dataset_size = 1000000
+dataset_size = 100000
 dim = 10
-dataset_file = "$SCRATCH/datasets/gaussian_c3_size" + str(dataset_size) + "_dim" + str(dim) + ".arff"
+dataset_file = "/scratch/snx1600/pfandedd/datasets/gaussian_c3_size" + str(dataset_size) + "_dim" + str(dim) + ".arff"
 # dataset_file = "datasets/gaussian_c3_size" + str(dataset_size) + "_dim" + str(dim) + ".arff"
-level = 6
+level = 3
 lambda_value = 1E-6
 threshold = 0.7
 
@@ -19,10 +19,9 @@ nodes = nodes_start
 # for nodes in range(nodes_start, nodes_end):
 while nodes <= nodes_end:
     gpu_constraint = "-C gpumodedefault"
-    cmd_raw = "srun -N " + str(nodes + 1) + " " + gpu_constraint + " --time 00:10:00 --output=clustering_results/piz_daint_" + str(dataset_size) +"s_" + str(nodes) + "N.log --error=clustering_results/piz_daint_" + str(dataset_size) +"s_" + str(nodes) + "N.log_err ./datadriven/examplesMPI/distributed_clustering_cmd --config=" + config_file + " --threshold=" + str(threshold) + " --datasetFileName=" + dataset_file + "  --level=" + str(level) + " --lambda=" + str(lambda_value) + " --verbose_timers=true"
+    cmd_raw = "srun -N " + str(nodes + 1) + " " + gpu_constraint + " --time 00:10:00 --output=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log --error=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log_err ./datadriven/examplesMPI/distributed_clustering_cmd --config=" + config_file + " --threshold=" + str(threshold) + " --datasetFileName=" + dataset_file + "  --level=" + str(level) + " --lambda=" + str(lambda_value) + " --verbose_timers=true"
     print(cmd_raw)
     cmd = shlex.split(cmd_raw)
     p = subprocess.Popen(cmd)
     p.communicate()
-
     nodes *= 2
