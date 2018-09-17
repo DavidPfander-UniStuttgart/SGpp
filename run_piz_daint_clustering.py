06d328e2489bbff34ae3c6a4347ce0b7eed26ad4
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-09-17 16:16:26 +0200

    last piz daints settings in script

--- run_piz_daint_clustering.py
+++ run_piz_daint_clustering.py
@@ -3,8 +3,8 @@
 import subprocess, shlex
 
 # master node is added automatically
-nodes_start = 32
-nodes_end = 32
+nodes_start = 128
+nodes_end = 128
 config_file = "config_ocl_float_P100.cfg"
 # config_file = "config_ocl_float_XeonE31585v5.cfg"
 # dataset_sizes = [10000000, 100000000]
@@ -23,7 +23,7 @@ for dataset_size in dataset_sizes:
     # for nodes in range(nodes_start, nodes_end):
     while nodes <= nodes_end:
         gpu_constraint = "-C gpumodedefault"
-        cmd_raw = "sbatch -N " + str(nodes + 1) + " " + gpu_constraint + " --time 01:00:00 --output=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log --error=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log_err --wrap=\"srun ./datadriven/examplesMPI/distributed_clustering_cmd --config=" + config_file + " --threshold=" + str(threshold) + " --datasetFileName=" + dataset_file + " --epsilon " + str(epsilon) + "  --level=" + str(level) + " --lambda=" + str(lambda_value) + " --verbose_timers=true\""
+        cmd_raw = "sbatch -N " + str(nodes + 1) + " " + gpu_constraint + " --time 00:25:00 --output=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log --error=clustering_results/piz_daint_" + str(level) + "l_" + str(dataset_size) +"s_" + str(nodes) + "N.log_err --wrap=\"srun ./datadriven/examplesMPI/distributed_clustering_cmd --config=" + config_file + " --threshold=" + str(threshold) + " --datasetFileName=" + dataset_file + " --epsilon " + str(epsilon) + "  --level=" + str(level) + " --lambda=" + str(lambda_value) + " --verbose_timers=true\""
         print(cmd_raw)
         cmd = shlex.split(cmd_raw)
         p = subprocess.Popen(cmd)
