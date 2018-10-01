8c4529a9089c82f6232f08d0f48eeff8d2fbcb11
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2018-10-01 16:49:03 +0200

    up

---
+++ diss_experiments/regression_AutoTuneTMP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source source_autotunetmp_generic.sh
+
+./datadriven/examplesAutoTuneTMP/tune_mult_AutoTuneTMP --datasetFileName ../datasets/DR5/DR5_nowarnings_less05_train.arff --level 6 --scenarioName tune_mult_AutoTuneTMP --tuner_name bruteforce --repetitions 5
+./datadriven/examplesAutoTuneTMP/tune_mult_AutoTuneTMP --datasetFileName ../datasets/DR5/DR5_nowarnings_less05_train.arff --level 6 --scenarioName tune_mult_AutoTuneTMP --tuner_name line_search --repetitions 5
+./datadriven/examplesAutoTuneTMP/tune_mult_AutoTuneTMP --datasetFileName ../datasets/DR5/DR5_nowarnings_less05_train.arff --level 6 --scenarioName tune_mult_AutoTuneTMP --tuner_name neighborhood_search --repetitions 5
+./datadriven/examplesAutoTuneTMP/tune_mult_AutoTuneTMP --datasetFileName ../datasets/DR5/DR5_nowarnings_less05_train.arff --level 6 --scenarioName tune_mult_AutoTuneTMP --tuner_name monte_carlo --repetitions 5
+mv tune_mult_AutoTuneTMP_host_${HOSTNAME}_* ${ALL_DATA_REPO_ROOT_PATH}/sparse_grids/regression/
