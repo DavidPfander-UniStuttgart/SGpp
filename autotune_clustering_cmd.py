67d7ce0f2ecb16a190270f0bca55b92171f9db95
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-12-19 17:59:06 +0100

    Analysis script produces correct output and recycles whereever possible

--- autotune_clustering_cmd.py
+++ autotune_clustering_cmd.py
@@ -10,7 +10,8 @@ import filecmp
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Autotunes the clustering parameter lambda and\
-    threshold over a given range for a specific dataset')
+    threshold over a given range for a specific dataset',
+                                     epilog='Example call: python3 autotune_clustering_cmd.py --dataset test.arff --reference_file test-erg.txt --grid_level 4 --config datadriven/examplesOCL/MyOCLConf.cfg --k 8 --lambda_list 0.2 0.1 0.01 0.001 0.0001 --threshold_list 0.0 0.00001 0.0001 0.001 0.01 0.1 1.0 --verbose=True --print_cluster_threshold=0')
     parser.add_argument('--dataset', type=str, required=True,
                         help='[filename] File containing the dataset.')
     parser.add_argument('--reference_file', type=str, required=True,
@@ -21,6 +22,11 @@ if __name__ == '__main__':
                         help=' [filename] OpenCL configuration file.')
     parser.add_argument('--k', type=int, required=True,
                         help=' [int] Number of considered neighbors for the KNN algorithm.')
+    parser.add_argument('--verbose', type=bool, required=False, default=True,
+                        help=' [boolean] Print verbose analysis instead of just the overall hitrate)')
+    parser.add_argument('--print_cluster_threshold', type=int, required=False, default=20,
+                        help=' [int] Do not print clusters containing less data points than this \
+                        threshold. Instead they will be summarized as one entry. ')
     parser.add_argument('--lambda_list', nargs='+', help='Lambda values that will be tested', required=True)
     parser.add_argument('--threshold_list', nargs='+', help='Threshold values that will be tested', required=True)
     args = parser.parse_args()
@@ -37,26 +43,58 @@ if __name__ == '__main__':
     dataset_arg = "--datasetFileName=" + args.dataset
     config_arg = "--config=" + args.config
     level_arg = "--level=" + str(args.grid_level)
+    k_arg = "--k=" + str(args.k)
+    reuse_graph_arg = ""
+    reuse_grid_arg = ""
+    reuse_coef_arg = ""
+    results = []
+    crashed = []
     for lambda_value in args.lambda_list:
+        reuse_grid_arg = ""
+        reuse_coef_arg = "" # dont reuse coefficients with new lambda
         lambda_arg = "--lambda=" + str(lambda_value)
         for threshold_value in args.threshold_list:
             threshold_arg = "--threshold=" + str(threshold_value)
-            print("Running with lambda: ", lambda_value, " threshold: ", threshold_value)
-            subprocess.run(["datadriven/examplesOCL/clustering_cmd", dataset_arg, "--k=5",
-                            config_arg, level_arg, "--epsilon=0.001", threshold_arg, lambda_arg,
-                            "--write_all",
-                            "--knn_algorithm=naive_ocl", "--scenario_prefix=test"],
-                            stdout=subprocess.PIPE)
-            print("Run finished")
-            results_file = "test_cluster_map.csv"
-            if not os.path.exists(results_file):
-                print("Error! File", results_file, " does not exist. Exiting...")
-                sys.exit(1)
-            print("Counting hits...")
-            actual_assignement = np.genfromtxt(results_file, delimiter=',', usecols=(-1))
-            counter_correct = count_correct_cluster_hits(reference_assignement, actual_assignement,
-                                                        False, False,
-                                                         False)
-            # Human readable form
-            percentage = round(counter_correct/ reference_assignement.shape[0] * 100.0, 4)
-            print("Finished! Overall hitrate is: ", percentage)
+            if args.verbose:
+                print("==================================================================")
+            print("Running new scenario with lambda: ", lambda_value, " threshold: ", threshold_value, " ...")
+            ret = subprocess.call(["datadriven/examplesOCL/clustering_cmd", dataset_arg, k_arg,
+                                   config_arg, level_arg, "--epsilon=0.001", threshold_arg, lambda_arg,
+                                   "--write_all", reuse_graph_arg, reuse_grid_arg, reuse_coef_arg,
+                                   "--knn_algorithm=naive_ocl", "--file_prefix=test"],
+                                  stdout=subprocess.PIPE)
+            print("datadriven/examplesOCL/clustering_cmd", dataset_arg, k_arg,
+                                   config_arg, level_arg, "--epsilon=0.001", threshold_arg, lambda_arg,
+                                   "--write_all", reuse_graph_arg, reuse_grid_arg, reuse_coef_arg,
+                                   "--knn_algorithm=naive_ocl", "--file_prefix=test", sep=' ')
+            if ret is 0:
+                results_file = "test_cluster_map.csv"
+                if not os.path.exists(results_file):
+                    print("Error! File", results_file, " does not exist. Exiting...")
+                    sys.exit(1)
+                actual_assignement = np.genfromtxt(results_file, delimiter=',', usecols=(-1))
+                counter_correct = count_correct_cluster_hits(reference_assignement, actual_assignement,
+                                                            args.verbose, args.print_cluster_threshold)
+                # Human readable form
+                percentage = round(counter_correct/ reference_assignement.shape[0] * 100.0, 4)
+                results.append((percentage, (lambda_value, threshold_value)))
+                if args.verbose:
+                    print("------------------------------------------------------------------")
+                print("Finished! Correct assignements: ", counter_correct, " => Overall hitrate is: ", percentage)
+                reuse_graph_arg = "--reuse_knn_graph=test_graph.csv"
+                reuse_grid_arg = "--reuse_density_grid=test_density_grid.serialized"
+                reuse_coef_arg = "--reuse_density_coef=test_density_coef.serialized"
+            else:
+                print("==> Crashed with this configuration! <==")
+                crashed.append((lambda_value, threshold_value))
+
+    sorted_results = sorted(results, key = lambda x : x[0], reverse = True)
+    print("==================================================================")
+    print("==================================================================")
+    print("Results for different parameters:")
+    for result in sorted_results:
+        print("-> Hitrate ", result[0], " %% with lambda ", result[1][0], " and threshold ", result[1][1])
+    print("==================================================================")
+    print("Crashed scenarios:")
+    for result in crashed:
+        print("-> Crashed with lambda ", result[0], " and threshold ", result[1])
--- check_cluster_assignements.py
+++ check_cluster_assignements.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 import sys
 
-def count_correct_cluster_hits(correct_output, actual_output, print_detailed_hitrate,
-                               print_ID_assignement, print_cluster_threshold):
+def count_correct_cluster_hits(correct_output, actual_output, verbose,
+                               print_cluster_threshold):
     # Now count the correct hits .. to do so we must first assigne the
     # clustering IDs from the dataset to the IDs our clustering program produced
 
@@ -22,15 +22,17 @@ def count_correct_cluster_hits(correct_output, actual_output, print_detailed_hit
     found_sizes[-2] = 0 # if even we have no removed data points we want the bin to exist to avoid ifs
     for i in range(0, actual_output.shape[0]):
         found_sizes[actual_output[i]] =  found_sizes[actual_output[i]] + 1
-    print("------------------------------------------------------------------")
-    print("Datamining algorithm found ", found_cluster_ids.size - 1, " differenct cluster IDs:")
+    if verbose:
+        print("------------------------------------------------------------------")
+        print("Datamining algorithm found ", found_cluster_ids.size - 1, " differenct cluster IDs:")
     sum_skipped_clusters = 0
     sum_skipped_datapoints = 0
     for ID in found_cluster_ids:
         # ignore removed data points
         if ID != -2:
             if found_sizes[ID] >= print_cluster_threshold:
-                print("Size of detected cluster with ID ", ID, ": ", found_sizes[ID])
+                if verbose:
+                    print("Size of detected cluster with ID ", (int)(ID), ": ", found_sizes[ID])
             else:
                 sum_skipped_clusters = sum_skipped_clusters + 1
                 sum_skipped_datapoints = sum_skipped_datapoints + found_sizes[ID]
@@ -68,32 +70,37 @@ def count_correct_cluster_hits(correct_output, actual_output, print_detailed_hit
     for cluster in cluster_ids:
         if cluster == -1: # -1 will be handles later -> hard code assignement with -2
             continue
-        current_maximum = -1
+        current_maximum = -2
         to_remove_ID = (0, 0)
         # find current best bin
+        found_bin = False
         for (x,y) in bins:
-            if bins[(x,y)] > current_maximum and (int)(y) != -2 and (int)(x) != -1: # ignore mappings onto removed data point "cluster"
+            if (int)(y) == -2 or (int)(x) == -1:
+                continue
+            if bins[(x,y)] > current_maximum : # ignore mappings onto removed data point "cluster"
                 current_maximum = bins[(x,y)]
                 combination_ID = (x,y)
                 correct_assignements[x] = y
                 to_remove_ID = (x, y)
-        # Add this bin to correct hits
-        hits[to_remove_ID[0]] = current_maximum
-        counter_correct = counter_correct + current_maximum
-        # Remove all assignement combinations made impossible by this assignement
-        to_remove_list = []
-        for (x,y) in bins:
-            if x == to_remove_ID[0] or y == to_remove_ID[1]:
-                to_remove_list.append((x,y)) #cannot del in bins whilst iterating
-        for d in to_remove_list:
-            del bins[d] # but we can delete in this loop
+                found_bin = True
+        if found_bin:
+            # Add this bin to correct hits
+            hits[to_remove_ID[0]] = current_maximum
+            counter_correct = counter_correct + current_maximum
+            # Remove all assignement combinations made impossible by this assignement
+            to_remove_list = []
+            for (x,y) in bins:
+                if x == to_remove_ID[0] or y == to_remove_ID[1]:
+                    to_remove_list.append((x,y)) #cannot del in bins whilst iterating
+            for d in to_remove_list:
+                del bins[d] # but we can delete in this loop
 
-    if print_ID_assignement:
+    if verbose:
         print("------------------------------------------------------------------")
         print("Mapping from reference cluster ID to detected cluster ID:")
         print(correct_assignements)
 
-    if print_detailed_hitrate:
+    if verbose:
         # Get size of clusters from reference file to output percentage of hits
         sizes = {}
         for ID in cluster_ids:
@@ -108,7 +115,7 @@ def count_correct_cluster_hits(correct_output, actual_output, print_detailed_hit
                     print("Size of reference cluster ", ID, " is not greater than 0. This should not happen! Exiting ...")
                     sys.exit(1)
                 percentage = round(hits[ID] / sizes[ID] * 100.0, 4)
-                print("Reference cluster ", ID, " hit rate is ", hits[ID], " hits out of ", sizes[ID],
+                print("Reference cluster ", (int)(ID), " hit rate is ", hits[ID], " hits out of ", sizes[ID],
                       " data points => ", percentage, "%")
         percentage = 100.0
         if sizes[-1] > 0: #noise can be zero
@@ -130,18 +137,12 @@ if __name__ == '__main__':
     parser.add_argument('--print_cluster_threshold', type=int, required=False, default=20,
                         help=' [int] Do not print clusters containing less data points than this \
                         threshold. Instead they will be summarized as one entry. ')
-    parser.add_argument('--print_hitrate_per_cluster', type=str, required=False, default=True,
-                        help=' [boolean] Prints the hit rate per cluster (including the noise)')
-    parser.add_argument('--print_ID_mapping', type=str, required=False, default=True,
-                        help=' [boolean] Prints how the cluster IDs of the reference file are mapped onto \
-                        the cluster IDs of the other file. For example, cluster 1 could be \
-                        recognized as cluster 3 in the datamining algorithm, thus we would \
-                        print the mapping 1 : 3')
+    parser.add_argument('--verbose', type=bool, required=False, default=True,
+                        help=' [boolean] Print verbose analysis instead of just the overall hitrate)')
     args = parser.parse_args()
     reference_file = args.reference_cluster_assignement
     results_file = args.cluster_assignement
-    print_detailed_hitrate = args.print_hitrate_per_cluster
-    print_ID_mapping = args.print_ID_mapping
+    verbose = args.verbose
 
     # Check existence
     if not os.path.exists(reference_file):
@@ -162,7 +163,7 @@ if __name__ == '__main__':
 
     # Calculate hit rate
     counter_correct = count_correct_cluster_hits(reference_assignement, actual_assignement,
-                                                 print_detailed_hitrate, print_ID_mapping, args.print_cluster_threshold)
+                                                 verbose, args.print_cluster_threshold)
     # Human readable form
     percentage = round(counter_correct/ reference_assignement.shape[0] * 100.0, 4)
     print("------------------------------------------------------------------")
