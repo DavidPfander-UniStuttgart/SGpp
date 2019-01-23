8ad4af4af0c5535036b31128aa07a707388125b5
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2019-01-23 09:23:58 +0100

    update node-level perf script

--- clustering_scripts/node-level_quality_perf.sh
+++ clustering_scripts/node-level_quality_perf.sh
@@ -25,18 +25,7 @@ for run in $(seq 0 ${no_runs}); do
     ./datadriven/examplesOCL/clustering_cmd --config OCL_configs/config_ocl_float_${device_name}.cfg --binary_header_filename ${dataset_directory}/gaussian_c${clusters}_size${dataset_size}_dim10_noise --level=${level} --lambda=${lambda} --k 6 --epsilon 0.01 --threshold ${threshold} --print_cluster_sizes 1 --target_clusters ${clusters} > results_WPDM18/${device_name}_perf_quality_${file_suffix}.log 2>&1
 done
 
-# 1M, 10C, lambda=1E-5 t=667 l=6 epsilon=1E-2
-for run in $(seq 0 ${no_runs}); do
-    dataset_size=1000000
-    clusters=10
-    lambda=1E-5
-    threshold=667
-    level=6
-    file_suffix=${dataset_size}s_${clusters}c_${lambda}lam_${level}l_run${run}
-    ./datadriven/examplesOCL/clustering_cmd --config OCL_configs/config_ocl_float_${device_name}.cfg --binary_header_filename ${dataset_directory}/gaussian_c${clusters}_size${dataset_size}_dim10_noise --level=${level} --lambda=${lambda} --k 6 --epsilon 0.01 --threshold ${threshold} --print_cluster_sizes 1 --target_clusters ${clusters} > results_WPDM18/${device_name}_perf_quality_${file_suffix}.log 2>&1
-done
-
-# 1M, 100C, lambda=1E-6 t=556 l=7 epsilon=1E-2
+# 1M, 10C, lambda=1E-6 t=556 l=6 epsilon=1E-2
 for run in $(seq 0 ${no_runs}); do
     dataset_size=1000000
     clusters=100
@@ -44,7 +33,7 @@ for run in $(seq 0 ${no_runs}); do
     threshold=556
     level=7
     file_suffix=${dataset_size}s_${clusters}c_${lambda}lam_${level}l_run${run}
-    ./datadriven/examplesOCL/clustering_cmd --config OCL_configs/config_ocl_float_${device_name}.cfg --binary_header_filename ${dataset_directory}/gaussian_c${clusters}_size${dataset_size}_dim10_noise --level=${level} --lambda=${lambda} --k 6 --epsilon 0.01 --threshold ${threshold} --print_cluster_sizes 1 --target_clusters ${clusters} > results_WPDM18/${device_name}_perf_quality_run_${file_suffix}.log 2>&1
+    ./datadriven/examplesOCL/clustering_cmd --config OCL_configs/config_ocl_float_${device_name}.cfg --binary_header_filename ${dataset_directory}/gaussian_c${clusters}_size${dataset_size}_dim10_noise --level=${level} --lambda=${lambda} --k 6 --epsilon 0.01 --threshold ${threshold} --print_cluster_sizes 1 --target_clusters ${clusters} > results_WPDM18/${device_name}_perf_quality_${file_suffix}.log 2>&1
 done
 
 # 1M, 100C, lambda=1E-7 t=1056 l=7 epsilon=1E-2
