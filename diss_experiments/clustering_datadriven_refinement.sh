0d50ef8c6f5ca7f2d2a2e2a936a76882eebbd631
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2019-03-06 19:07:14 +0100

    adding data for projection graphs

--- diss_experiments/clustering_datadriven_refinement.sh
+++ diss_experiments/clustering_datadriven_refinement.sh
@@ -13,6 +13,8 @@ device="QuadroGP100"
 
 ./clustering_scripts/check_ARI.py --reference_cluster_assignment datasets_diss/gaussian_c100_size100000_dim10id5_noise_class.arff --cluster_assignment ref_testing_cluster_map.csv >> results_diss/clustering_adaptive_datadriven_dim10id5.log 2>&1
 
+mv ref_testing_grid_coord.csv results_diss/datadriven_ref_dim10id5_grid.csv
+
 # dim20id5
 ./datadriven/examplesOCL/clustering_cmd --datasetFileName datasets_diss/gaussian_c100_size100000_dim20id5_noise.arff --level 15 --epsilon 1E-2 --threshold 7.6E8 --lambda 7.8E-13 --k 6 --config OCL_configs/config_ocl_float_${device}.cfg --write_cluster_map --file_prefix ref_testing  --refinement_steps 0 --refinement_points 1000 --coarsen_points 0 --coarsen_threshold 1E-7 --max_iterations 200 --use_datadriven_refinement --datadriven_refinement_min_support 800 --write_density_grid > results_diss/clustering_adaptive_datadriven_dim20id5.log 2>&1
 
@@ -25,6 +27,11 @@ device="QuadroGP100"
 
 ./clustering_scripts/check_ARI.py --reference_cluster_assignment datasets_diss/gaussian_c100_size100000_dim5id5_noise_class.arff --cluster_assignment ref_testing_cluster_map.csv >> results_diss/clustering_adaptive_surplus_dim5id5.log 2>&1
 
+# special dim10id5 run to only obtain the grid, ARI is essentially 0
+./datadriven/examplesOCL/clustering_cmd --datasetFileName datasets_diss/gaussian_c100_size100000_dim5id5_noise.arff --level 7 --epsilon 1E-2 --threshold 13000.0 --lambda 3.0E-8 --k 6 --config OCL_configs/config_ocl_float_QuadroGP100.cfg --write_cluster_map --file_prefix ref_testing --refinement_steps 5 --refinement_points 800 --write_density_grid > results_diss/clustering_adaptive_surplus_dim10id5.log 2>&1
+mv ref_testing_grid_coord.csv results_diss/surplus_ref_dim10id5_grid.csv
+
+
 # dim5id5 with pruning only
 ./datadriven/examplesOCL/clustering_cmd --datasetFileName datasets_diss/gaussian_c100_size100000_dim5id5_noise.arff --level 11 --epsilon 1E-2 --threshold 54.0 --lambda 1.0E-5 --k 6 --config OCL_configs/config_ocl_float_QuadroGP100.cfg --write_cluster_map --file_prefix ref_testing --use_b_coarsening --b_coarsening_threshold 7.5E-4 --refinement_steps 0 --refinement_points 1000 --coarsen_points 0 --coarsen_threshold 5.0E-5 > results_diss/clustering_adaptive_pruning_dim5id5.log
 
@@ -34,3 +41,4 @@ device="QuadroGP100"
 ./datadriven/examplesOCL/clustering_cmd --datasetFileName datasets_diss/gaussian_c100_size100000_dim10id5_noise.arff --level 9 --epsilon 1E-2 --threshold 13000.0 --lambda 3.0E-8 --k 6 --config OCL_configs/config_ocl_float_QuadroGP100.cfg --write_cluster_map --file_prefix ref_testing --refinement_steps 0 --refinement_points 800 --max_iterations 200 --use_b_coarsening --b_coarsening_threshold 1E-4 --write_density_grid > results_diss/clustering_adaptive_pruning_dim10id5.log
 
 ./clustering_scripts/check_ARI.py --reference_cluster_assignment datasets_diss/gaussian_c100_size100000_dim10id5_noise_class.arff --cluster_assignment ref_testing_cluster_map.csv >> results_diss/clustering_adaptive_pruning_dim10id5.log 2>&1
+
