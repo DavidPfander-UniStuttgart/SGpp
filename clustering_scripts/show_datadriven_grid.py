3f618455d3ca665187a38f8be8964bbb71702e68
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2019-03-08 09:45:58 +0100

    up

--- clustering_scripts/show_datadriven_grid.py
+++ clustering_scripts/show_datadriven_grid.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import csv
 import re
 
-file_name = "refined_grid.csv"
-dataset_file_name = None
-df = pd.read_csv(file_name, comment='#') # names=['x', 'y'] header=None,
-# print(df)
+file_name = "results_diss/support_refined_grid_G2D.csv"
+dataset_file_name = "datasets_diss/gaussian_c4_size500_dim2id2_noise.arff"
+df = pd.read_csv(file_name, comment='#') # header=None, names=['x0', 'x1'] # names=['x', 'y'] header=None,
+print(df)
 # print(df.columns.values)
 
-with open(file_name) as f:
-    reader = csv.reader(f)
-    row1 = next(reader)
-    row2 = next(reader)
-    if not row2[0].startswith("#"):
-        raise
-    m = re.match(r"\#filename\: (.*?)$", row2[0])
-    if m == None:
-        raise
-    dataset_file_name = m.group(1)
+# with open(file_name) as f:
+#     reader = csv.reader(f)
+#     row1 = next(reader)
+#     row2 = next(reader)
+#     if not row2[0].startswith("#"):
+#         raise
+#     m = re.match(r"\#filename\: (.*?)$", row2[0])
+#     if m == None:
+#         raise
+#     dataset_file_name = m.group(1)
 
 print("dataset_file_name:", dataset_file_name)
 
 # df_dataset = pd.read_csv("ripley.arff", header=None, names=['x', 'y', 'target'], skiprows=7)
 # df_dataset = pd.read_csv("gaussian_c2_size1000_dim2.arff", header=None, names=['x', 'y', 'target'], skiprows=7)
-df_dataset = pd.read_csv(dataset_file_name, header=None, names=['x', 'y', 'target'], skiprows=7)
+df_dataset = pd.read_csv(dataset_file_name, header=None, names=['x0', 'x1', 'target'], skiprows=6)
 # print(df_dataset)
 
-plt.scatter(df_dataset['x'], df_dataset['y'])
+plt.scatter(df_dataset['x0'], df_dataset['x1'])
 plt.scatter(df['x0'], df[' x1'])
 plt.xlim((0.0, 1.0))
 plt.ylim((0.0, 1.0))
