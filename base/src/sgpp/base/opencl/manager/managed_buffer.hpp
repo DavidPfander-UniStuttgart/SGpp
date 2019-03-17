ffebfa90fedb294a49c6d6a7ef549625285a8f5f
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2019-03-17 13:42:53 +0100

    multi device support refinement

--- base/src/sgpp/base/opencl/manager/managed_buffer.hpp
+++ base/src/sgpp/base/opencl/manager/managed_buffer.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
+#include "device.hpp"
+#include "manager.hpp"
 #include <CL/cl.h>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <vector>
-#include "device.hpp"
-#include "manager.hpp"
 
 namespace opencl {
 
-template <class T>
-class managed_buffer {
- private:
+template <class T> class managed_buffer {
+private:
   device_t device;
   cl_mem ptr;
   size_t buffer_size;
 
- public:
+public:
   managed_buffer() : ptr(nullptr), buffer_size(0){};
 
   managed_buffer(device_t &device, const int buffer_size)
       : device(device), buffer_size(buffer_size) {
     cl_int err;
-    ptr = clCreateBuffer(device.context, CL_MEM_READ_WRITE, sizeof(T) * buffer_size, nullptr, &err);
+    ptr = clCreateBuffer(device.context, CL_MEM_READ_WRITE,
+                         sizeof(T) * buffer_size, nullptr, &err);
     opencl::check(err, "managed_buffer: clCreateBuffer failed");
   }
   managed_buffer(const managed_buffer &other) = delete;
@@ -55,37 +55,49 @@ class managed_buffer {
 
   cl_mem get() { return ptr; }
 
-  void to_device(const std::vector<T> &data) { to_device(data, 0, data.size()); }
+  void to_device(const std::vector<T> &data) {
+    to_device(data, 0, data.size());
+  }
 
-  void to_device(const std::vector<T> &data, const size_t range_start, const size_t range_end) {
+  void to_device(const std::vector<T> &data, const size_t range_start,
+                 const size_t range_end) {
     if (!ptr) {
       throw std::runtime_error("managed_buffer: buffer not initialized");
     }
     const size_t range = range_end - range_start;
     if (range > buffer_size) {
-      throw std::runtime_error("managed_buffer: to_device: device buffer too small");
+      throw std::runtime_error(
+          "managed_buffer: to_device: device buffer too small");
     }
     cl_int err;
-    err = clEnqueueWriteBuffer(device.commandQueue, ptr, CL_TRUE, 0, sizeof(T) * range,
-                               data.data() + range_start, 0, nullptr, nullptr);
+    err = clEnqueueWriteBuffer(device.commandQueue, ptr, CL_TRUE, 0,
+                               sizeof(T) * range, data.data() + range_start, 0,
+                               nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueWriteBuffer failed");
     clFinish(device.commandQueue);
   }
 
   void from_device(std::vector<T> &data) { from_device(data, 0, data.size()); }
 
-  void from_device(std::vector<T> &data, const size_t range_start, const size_t range_end) {
+  void from_device(std::vector<T> &data, const size_t range_start,
+                   const size_t range_end) {
     if (!ptr) {
       throw std::runtime_error("managed_buffer: buffer not initialized");
     }
+    if (range_end > data.size()) {
+      throw std::runtime_error(
+          "managed_buffer: range_end beyond supplied host buffer size");
+    }
     size_t range = static_cast<size_t>(range_end - range_start);
+
     // if (data.size() < buffer_size) {
-    //   throw std::runtime_error("managed_buffer: from_device: host-side buffer too small");
+    //   throw std::runtime_error("managed_buffer: from_device: host-side buffer
+    //   too small");
     // }
     cl_int err;
     err = clEnqueueReadBuffer(device.commandQueue, ptr, CL_TRUE, 0,
-                              sizeof(T) * std::min(buffer_size, range), data.data() + range_start,
-                              0, nullptr, nullptr);
+                              sizeof(T) * std::min(buffer_size, range),
+                              data.data() + range_start, 0, nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueReadBuffer failed");
     clFinish(device.commandQueue);
   }
@@ -95,8 +107,8 @@ class managed_buffer {
       throw std::runtime_error("managed_buffer: buffer not initialized");
     }
     cl_int err;
-    err = clEnqueueFillBuffer(device.commandQueue, ptr, &value, sizeof(value), 0,
-                              buffer_size * sizeof(T), 0, nullptr, nullptr);
+    err = clEnqueueFillBuffer(device.commandQueue, ptr, &value, sizeof(value),
+                              0, buffer_size * sizeof(T), 0, nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueFillBuffer failed");
     clFinish(device.commandQueue);
   }
@@ -104,21 +116,20 @@ class managed_buffer {
   size_t size() { return buffer_size; }
 };
 
-template <>
-class managed_buffer<bool> {
- private:
+template <> class managed_buffer<bool> {
+private:
   device_t device;
   cl_mem ptr;
   size_t buffer_size;
 
- public:
+public:
   managed_buffer() : ptr(nullptr), buffer_size(0){};
 
   managed_buffer(device_t &device, const int buffer_size)
       : device(device), buffer_size(buffer_size) {
     cl_int err;
-    ptr = clCreateBuffer(device.context, CL_MEM_READ_WRITE, sizeof(unsigned char) * buffer_size,
-                         nullptr, &err);
+    ptr = clCreateBuffer(device.context, CL_MEM_READ_WRITE,
+                         sizeof(unsigned char) * buffer_size, nullptr, &err);
     opencl::check(err, "managed_buffer: clCreateBuffer failed");
   }
   managed_buffer(const managed_buffer &other) = delete;
@@ -150,15 +161,19 @@ class managed_buffer<bool> {
 
   cl_mem get() { return ptr; }
 
-  void to_device(const std::vector<bool> &data) { to_device(data, 0, data.size()); }
+  void to_device(const std::vector<bool> &data) {
+    to_device(data, 0, data.size());
+  }
 
-  void to_device(const std::vector<bool> &data, const size_t range_start, const size_t range_end) {
+  void to_device(const std::vector<bool> &data, const size_t range_start,
+                 const size_t range_end) {
     if (!ptr) {
       throw std::runtime_error("managed_buffer: buffer not initialized");
     }
     size_t range = range_end - range_start;
     if (range > buffer_size) {
-      throw std::runtime_error("managed_buffer: to_device: device buffer too small");
+      throw std::runtime_error(
+          "managed_buffer: to_device: device buffer too small");
     }
 
     std::vector<unsigned char> data_temp(range);
@@ -168,30 +183,39 @@ class managed_buffer<bool> {
 
     cl_int err;
     err = clEnqueueWriteBuffer(device.commandQueue, ptr, CL_TRUE, 0,
-                               sizeof(unsigned char) * data_temp.size(), data_temp.data(), 0,
-                               nullptr, nullptr);
+                               sizeof(unsigned char) * data_temp.size(),
+                               data_temp.data(), 0, nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueWriteBuffer failed");
     clFinish(device.commandQueue);
   }
 
-  void from_device(std::vector<bool> &data) { from_device(data, 0, data.size()); }
+  void from_device(std::vector<bool> &data) {
+    from_device(data, 0, data.size());
+  }
 
-  void from_device(std::vector<bool> &data, const size_t range_start, const size_t range_end) {
+  void from_device(std::vector<bool> &data, const size_t range_start,
+                   const size_t range_end) {
     if (!ptr) {
       throw std::runtime_error("managed_buffer: buffer not initialized");
     }
     size_t range = range_end - range_start;
+    if (range_end > data.size()) {
+      throw std::runtime_error(
+          "managed_buffer: range_end beyond supplied host buffer size");
+    }
     // if (data.size() < buffer_size) {
-    //   throw std::runtime_error("managed_buffer: from_device: host-side buffer too small");
+    //   throw std::runtime_error("managed_buffer: from_device: host-side buffer
+    //   too small");
     // }
     std::vector<unsigned char> data_temp(range);
     cl_int err;
     err = clEnqueueReadBuffer(device.commandQueue, ptr, CL_TRUE, 0,
-                              sizeof(unsigned char) * std::min(buffer_size, range),
+                              sizeof(unsigned char) *
+                                  std::min(buffer_size, range),
                               data_temp.data(), 0, nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueReadBuffer failed");
     clFinish(device.commandQueue);
-    for (size_t i = 0; i < data.size(); i += 1) {
+    for (size_t i = 0; i < std::min(buffer_size, range); i += 1) {
       data[range_start + i] = data_temp[i];
     }
   }
@@ -202,8 +226,9 @@ class managed_buffer<bool> {
     }
     unsigned char value_temp = value;
     cl_int err;
-    err = clEnqueueFillBuffer(device.commandQueue, ptr, &value_temp, sizeof(unsigned char), 0,
-                              buffer_size * sizeof(unsigned char), 0, nullptr, nullptr);
+    err = clEnqueueFillBuffer(
+        device.commandQueue, ptr, &value_temp, sizeof(unsigned char), 0,
+        buffer_size * sizeof(unsigned char), 0, nullptr, nullptr);
     opencl::check(err, "managed_buffer: clEnqueueFillBuffer failed");
     clFinish(device.commandQueue);
   }
@@ -211,4 +236,4 @@ class managed_buffer<bool> {
   size_t size() { return buffer_size; }
 };
 
-}  // namespace opencl
+} // namespace opencl
--- datadriven/src/sgpp/datadriven/grid/support_refinement_iterative_OCL.cpp
+++ datadriven/src/sgpp/datadriven/grid/support_refinement_iterative_OCL.cpp
@@ -12,30 +12,45 @@ namespace sgpp::datadriven {
 
 void support_refinement_iterative::verify_support_blocked_OCL() {
   int64_t num_candidates = static_cast<int64_t>(schedule_level.size()) / dim;
-  int64_t num_candidates_padded = num_candidates;
-  if (num_candidates % block_size != 0) {
-    num_candidates_padded += block_size - (num_candidates % block_size);
-  }
-  int64_t blocks_total = num_candidates_padded / block_size;
+  // int64_t blocks_total = num_candidates_padded / block_size;
   // truncated number of blocks per device, potentially larger schedule for last
   // device
-  int64_t blocks_per_device = blocks_total / manager->get_devices().size();
+  // int64_t blocks_per_device = blocks_total / manager->get_devices().size();
+  int64_t num_candidates_per_device =
+      num_candidates / manager->get_devices().size();
 
   std::cout << "num_candidates: " << num_candidates
-            << " num_candidates_padded: " << num_candidates_padded
+            // << " num_candidates_padded: " << num_candidates_padded
             << " block_size: " << block_size
-            << " blocks_per_device: " << blocks_per_device << std::endl;
+            << " num_candidates_per_device: " << num_candidates_per_device
+            << std::endl;
+// << " blocks_per_device: " << blocks_per_device << std::endl;
 
+#pragma omp parallel for
   for (size_t i = 0; i < manager->get_devices().size(); i += 1) {
-    int64_t range_start = i * blocks_per_device * block_size;
-    int64_t range_end = (i + 1) * blocks_per_device * block_size; // exclusive
+    int64_t range_start = i * num_candidates_per_device;
+    int64_t range_end = (i + 1) * num_candidates_per_device; // exclusive
+    // special case if there are too few work items for multiple GPUs
+    if (num_candidates < 4096) {
+      if (i == manager->get_devices().size() - 1) {
+        range_start = 0;
+      } else {
+        continue;
+      }
+    }
     // last device gets remaining work items
     if (i == manager->get_devices().size() - 1) {
-      range_end = num_candidates_padded;
+      range_end = num_candidates;
     }
+
     int64_t range_size = range_end - range_start;
 
-    std::cout << "device: " << i << " range_size: " << range_size
+    int64_t range_padded = range_size;
+    if (range_padded % block_size != 0) {
+      range_padded += block_size - (range_padded % block_size);
+    }
+
+    std::cout << "device: " << i << " range_padded: " << range_padded
               << " range_start: " << range_start << " range_end: " << range_end
               << std::endl;
     if (range_end <= 0) {
@@ -43,35 +58,35 @@ void support_refinement_iterative::verify_support_blocked_OCL() {
     }
 
     opencl::managed_buffer<long> schedule_level_device(
-        manager->get_devices()[i], range_size * dim);
+        manager->get_devices()[i], range_padded * dim);
     schedule_level_device.fill_buffer(1);
     schedule_level_device.to_device(schedule_level, range_start * dim,
                                     range_end * dim); // requires range
 
     opencl::managed_buffer<long> schedule_index_device(
-        manager->get_devices()[i], range_size * dim);
+        manager->get_devices()[i], range_padded * dim);
     schedule_index_device.fill_buffer(1);
     schedule_index_device.to_device(schedule_index, range_start * dim,
                                     range_end * dim); // requires range
 
     opencl::managed_buffer<bool> schedule_support_device(
-        manager->get_devices()[i], range_size);
+        manager->get_devices()[i], range_padded);
     schedule_support_device.fill_buffer(false);
 
-    if (!kernels_verify_support[i]) {
-      std::cout << "kernel is nullptr!" << std::endl;
-    }
     opencl::apply_arguments(kernels_verify_support[i], data_devices[i]->get(),
                             entries, schedule_level_device.get(),
                             schedule_index_device.get(),
                             schedule_support_device.get(), min_support);
 
     opencl::run_kernel_1d_timed(this->manager->get_devices()[i],
-                                kernels_verify_support[i], range_size,
+                                kernels_verify_support[i], range_padded,
                                 block_size);
 
-    schedule_support_device.from_device(schedule_support, range_start,
-                                        range_end);
+#pragma omp critical (support_refinement_multi_device)
+    {
+      schedule_support_device.from_device(schedule_support, range_start,
+                                          range_end);
+    }
   }
   // std::cout << "schedule_support: ";
   // for (size_t i = 0; i < schedule_support.size(); i += 1) {
